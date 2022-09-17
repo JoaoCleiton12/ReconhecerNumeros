c6eb42c9ac5dc5c614d40c1e8f6f4f2e142415ab
Joao Cleiton <joaocleiton60359@gmail.com>
2022-09-17 10:29:14 -0300

    first commit

---
+++ Principal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	/*
+	números escolhidos para representar as duas saída:
+	Primeiro número = 5 (cinco)
+	JOAO
+	2+1+1+1 = 5
+
+	Segundo número = 8 (oito)
+	NUNES
+	2+1+2+1+2 = 8
+
+	Os demais números serão parte da saída desconhecida:
+	Saída desconhecida = 0,1,2,3,4,6,7,9
+	*/
+
+	//se for 5 ou 8 irá entrar para verificar qual dos dois número é.
+	if () {
+		//se for 5 irá entrar aqui
+		if () {
+			cout << "Primeiro número.\n";
+		}
+		//se não for o 5, então é o 8 
+		else{
+			cout << "Segundo número.\n";
+		}
+	}
+	//se não for 5 ou 8, então é um número desconhecido.
+	else () {
+		cout << "Numero nao reconhecido\n";
+	}
+}
