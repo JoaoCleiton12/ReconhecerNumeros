a4dd2bb6a2fc0e3f352d1bb76cc1644349ba72f3
melkrn <89106204+melkrn@users.noreply.github.com>
2022-09-18 14:26:11 -0300

    Corrigir números que serão reconhecidos

--- Principal.cpp
+++ Principal.cpp
@@ -9,26 +9,26 @@ int main() {
 	JOAO
 	2+1+1+1 = 5
 
-	Segundo número = 8 (oito)
-	NUNES
-	2+1+2+1+2 = 8
+	Segundo número = 7 (sete)
+	MELQUISEDEQUE
+	(6*2+7)/3 = 6,33 = 7
 
 	Os demais números serão parte da saída desconhecida:
 	Saída desconhecida = 0,1,2,3,4,6,7,9
 	*/
 
-	//se for 5 ou 8 irá entrar para verificar qual dos dois número é.
+	//se for 5 ou 7 irá entrar para verificar qual dos dois número é.
 	if () {
 		//se for 5 irá entrar aqui
 		if () {
 			cout << "Primeiro número.\n";
 		}
-		//se não for o 5, então é o 8 
+		//se não for o 5, então é o 7 
 		else{
 			cout << "Segundo número.\n";
 		}
 	}
-	//se não for 5 ou 8, então é um número desconhecido.
+	//se não for 5 ou 7, então é um número desconhecido.
 	else () {
 		cout << "Numero nao reconhecido\n";
 	}
